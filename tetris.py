20ffe43727238b565b97a46be2bc1ade4ebaec4c
Samuel Sherwood <samuel.r.sherwood@gmail.com>
2026-06-11 16:34:35 -0700

    Updated single click logic, changed input variables in movement functions

--- tetris.py
+++ tetris.py
@@ -47,47 +47,45 @@ def render_window(frame):
                 line += "[ ]"
         print(line)
 
-def vertical_movement(frame, piece_row, piece_col, game_interval): # sexiest function I ever saw
+def vertical_movement(frame, piece_row, piece_col): # sexiest function I ever saw
     piece_row += 1
     frame[piece_row - 1][piece_col] = 0
     frame[piece_row][piece_col] = 1
     return piece_row
 
-def lateral_movement(key, frame, piece_row, piece_col): # just for coords changes
+def lateral_movement(right_pressed, left_pressed, frame, piece_row, piece_col): # just for coords changes
     if piece_col != 9 and frame[piece_row][piece_col + 1] != 1:
-        if key == 'right':
+        if right_pressed:
             piece_col += 1
             frame[piece_row][piece_col - 1] = 0
             frame[piece_row][piece_col] = 1
     if piece_col != 0 and frame[piece_row][piece_col - 1] != 1:
-        if key == 'left':
+        if left_pressed:
             piece_col -= 1
             frame[piece_row][piece_col + 1] = 0
             frame[piece_row][piece_col] = 1
     return piece_col
 
-def take_input(key, right_pressed, left_pressed): # total shit, need to add single key presses
+def take_input(key, left_pressed, right_pressed): # total shit, need to add single key presses
     if keyboard.is_pressed("left"):
-        key = "left"
         left_pressed = True
     if keyboard.is_pressed("right"):
-        key = "right"
         right_pressed = True
     if keyboard.is_pressed("esc"):
-        key = "esc"
+        pass
     return key, left_pressed, right_pressed
 
 # Do I gotta add all arguments or nah??
 def game_loop(key, frame, piece_row, piece_col, 
               game_interval, falling_interval, 
-              last_grav_time, right_pressed, 
-              left_pressed, right_previous_pressed, 
-              left_previous_pressed):
+              last_grav_time, left_pressed, 
+              right_pressed, left_previous_pressed, 
+              right_previous_pressed):
     while piece_row < 19 and frame[piece_row + 1][piece_col] != 1:
         current_time = time.time() # to do: gotta make this grav loop into a function
         difference = current_time - last_grav_time
         render_window(frame)
-        key, right_pressed, left_pressed = take_input(key, right_pressed, left_pressed) # no clue what variable this actually is
+        key, left_pressed, right_pressed = take_input(key, left_pressed, right_pressed)
 
         single_right = (right_pressed == True and right_previous_pressed == False)
         held_right = (right_pressed == True and right_previous_pressed == True)
@@ -96,31 +94,26 @@ def game_loop(key, frame, piece_row, piece_col,
 
         if single_right:  
             if difference >= falling_interval:
-                piece_row = vertical_movement(frame, piece_row, piece_col, falling_interval)
-                piece_col = lateral_movement(key, frame, piece_row, piece_col)
+                piece_row = vertical_movement(frame, piece_row, piece_col)
+                piece_col = lateral_movement(right_pressed, left_pressed, key, frame, piece_row, piece_col)
                 last_grav_time = current_time
-            right_previous_pressed = right_pressed
         if single_left:
             if difference >= falling_interval:
-                piece_row = vertical_movement(frame, piece_row, piece_col, falling_interval)
-                piece_col = lateral_movement(key, frame, piece_row, piece_col)
+                piece_row = vertical_movement(frame, piece_row, piece_col)
+                piece_col = lateral_movement(right_pressed, left_pressed, key, frame, piece_row, piece_col)
                 last_grav_time = current_time
-            left_previous_pressed = left_pressed
         if held_right: # Does not trigger at same time as line 97 because conditions are different
-            while held_right: 
-                if difference >= falling_interval:
-                    piece_row = vertical_movement(frame, piece_row, piece_col, falling_interval)
-                    piece_col = lateral_movement(key, frame, piece_row, piece_col)
-                    last_grav_time = current_time
-                right_previous_pressed = right_pressed
+            if difference >= falling_interval:
+                piece_row = vertical_movement(frame, piece_row, piece_col)
+                piece_col = lateral_movement(right_pressed, left_pressed, key, frame, piece_row, piece_col)                last_grav_time = current_time
+                last_grav_time = current_time        
         if held_left:
-            while held_left:
-                if difference >= falling_interval:
-                    piece_row = vertical_movement(frame, piece_row, piece_col, falling_interval)
-                    piece_col = lateral_movement(key, frame, piece_row, piece_col)
-                    last_grav_time = current_time
-                left_previous_pressed = left_pressed
-
+            if difference >= falling_interval:
+                piece_row = vertical_movement(frame, piece_row, piece_col)
+                piece_col = lateral_movement(right_pressed, left_pressed, key, frame, piece_row, piece_col)
+                last_grav_time = current_time
+        right_previous_pressed = right_pressed # Updating for single click
+        left_previous_pressed = left_pressed
         time.sleep(game_interval)  
         print("\033[H", end="") # Note to self: learn this shit
     return last_grav_time
