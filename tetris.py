f2d861190048f944d661d34038834cf0408efd71
Samuel Sherwood <samuel.r.sherwood@gmail.com>
2026-06-22 12:18:15 -0700

    Moved lateral movement booleans to lateral_movement function

--- tetris.py
+++ tetris.py
@@ -37,16 +37,25 @@ class TetrisGame:
         return self.piece_row
 
     def lateral_movement(self): # Horizontal movement
+        
+        single_right = (self.right_pressed and not self.right_previous_pressed)
+        held_right = (self.right_pressed and self.right_previous_pressed)
+        single_left = (self.left_pressed and not self.left_previous_pressed)
+        held_left = (self.left_pressed and self.left_previous_pressed)
+
         if self.piece_col != 9 and self.frame[self.piece_row + 1][self.piece_col] != 1:
-            if self.right_pressed:
+            if single_right:
                 self.piece_col += 1
                 self.frame[self.piece_row][self.piece_col - 1] = 0
                 self.frame[self.piece_row][self.piece_col] = 1
         if self.piece_col != 0 and self.frame[self.piece_row + 1][self.piece_col] != 1:
-            if self.left_pressed:
+            if self.single_left:
                 self.piece_col -= 1
                 self.frame[self.piece_row][self.piece_col + 1] = 0
                 self.frame[self.piece_row][self.piece_col] = 1
+
+        self.right_previous_pressed = self.right_pressed
+        self.left_previous_pressed = self.left_pressed
         return self.piece_col
     
     def game_loop(self):
@@ -56,42 +65,15 @@ class TetrisGame:
             self.render_window(self.frame)
             self.left_pressed, self.right_pressed = self.take_input(self.left_pressed, self.right_pressed)
 
-            
+            if difference >= self.falling_interval:
+                self.piece_row = self.vertical_movement(self.frame, self.piece_row, self.piece_col)
+                self.last_grav_time = current_time
 
-            single_right = (self.right_pressed == True and self.right_previous_pressed == False)
-            held_right = (self.right_pressed == True and self.right_previous_pressed == True)
-            single_left = (self.left_pressed == True and self.left_previous_pressed == False)
-            held_left = (self.left_pressed == True and self.left_previous_pressed == True)
-
-            if single_right:  
-                if difference >= self.falling_interval:
-                    self.piece_row = self.vertical_movement(self.frame, self.piece_row, self.piece_col)
-                    self.piece_col = self.lateral_movement(self.right_pressed, self.left_pressed, self.frame, self.piece_row, self.piece_col)
-                    self.last_grav_time = current_time
-            if single_left:  
-                if difference >= self.falling_interval:
-                    self.piece_row = self.vertical_movement(self.frame, self.piece_row, self.piece_col)
-                    self.piece_col = self.lateral_movement(self.right_pressed, self.left_pressed, self.frame, self.piece_row, self.piece_col)
-                    self.last_grav_time = current_time
-            if held_right:  
-                if difference >= self.falling_interval:
-                    self.piece_row = self.vertical_movement(self.frame, self.piece_row, self.piece_col)
-                    self.piece_col = self.lateral_movement(self.right_pressed, self.left_pressed, self.frame, self.piece_row, self.piece_col)
-                    self.last_grav_time = current_time
-            if held_left:  
-                if difference >= self.falling_interval:
-                    self.piece_row = self.vertical_movement(self.frame, self.piece_row, self.piece_col)
-                    self.piece_col = self.lateral_movement(self.right_pressed, self.left_pressed, self.frame, self.piece_row, self.piece_col)
-                    self.last_grav_time = current_time
-            self.right_previous_pressed = self.right_pressed # Updating for single click
-            self.left_previous_pressed = self.left_pressed
             time.sleep(self.game_interval)  
             print("\033[H", end="") # Note to self: learn this shit
         return self.last_grav_time
         
-            
-
 while True:
     game = TetrisGame()
-    print(game)
+    game.game_loop()
     
